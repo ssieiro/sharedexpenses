cb07d6a41b3aa14b919dd619d684db2ef0477137
Sonia Sieiro <soniasieirod@gmail.com>
2021-02-07 17:58:17 +0100

    setup project

---
+++ src/main/java/es/ssieiro/sharedexpenses/SharedexpensesApplication.java
@@ -0,0 +1,13 @@
+package es.ssieiro.sharedexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedexpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedexpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ssieiro/sharedexpenses/SharedexpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ssieiro.sharedexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedexpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
